9271032b0e4d6a5a1d5f2bfc7df83b6ca0cb2fa7
vn2089 <vn2089@nyu.edu>
2022-03-01 08:51:52 -0500

    Update terra.py

--- terra.py
+++ terra.py
@@ -1,7 +1,2 @@
 print("hello world")
 print("Whats up")
-mosen=random.randrange(1,6)
-if mosen<3:
-    print("The amount of moisture is 10 dewpoints")
-else:
-    print("The amount of moisture is 20 dewpoints")
